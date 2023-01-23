093df30c6f0888ecda5c2e80da43a043f775359b
leahjclark <101298556+leahjclark@users.noreply.github.com>
2023-01-23 10:43:01 -0500

    Add files via upload

---
+++ LIS 4370 Module 3.R
@@ -0,0 +1,13 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+CBS <- c(12, 75, 43, 19, 1, 21, 19) 
+data.frame(Name, ABC, CBS)
+##      Name ABC CBS
+## 1     Jeb   4  12
+## 2  Donald  62  75
+## 3     Ted  51  43
+## 4   Marco  21  19
+## 5   Carly   2   1
+## 6 Hillary  14  21
+## 7  Bernie  15  19
+
